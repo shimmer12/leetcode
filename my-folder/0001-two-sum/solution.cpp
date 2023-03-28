a62b0125045c5935b02073c5bcb8d40e6e8b1a1e
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2023-03-28 17:24:15 +0000

    [LeetCode Sync] Runtime - 382 ms (5.10%), Memory - 10.2 MB (99.99%)

--- my-folder/0001-two-sum/solution.cpp
+++ my-folder/0001-two-sum/solution.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> indices;
-        for (int i = 0; i < nums.size(); i++) {
-            if (indices.find(target - nums[i]) != indices.end()) {
-                return {indices[target - nums[i]], i};
-            }
-            indices[nums[i]] = i;
+        for(int i=0;i<nums.size();i++)
+        for(int j=1+i;j<nums.size();j++){
+       if( nums[i]+nums[j]==target)
+         return {i,j};
+       
         }
-        return {};
+       return {}; 
     }
+
 };
+
