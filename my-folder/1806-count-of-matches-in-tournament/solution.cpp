e2ae8cacffb7a77c64c768b92204bcf9496cf452
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2023-12-05 09:47:37 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.2 MB (100.00%)

---
+++ my-folder/1806-count-of-matches-in-tournament/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numberOfMatches(int n) {
+        int match=0;
+        while(n>=2){
+        if(n%2==0){
+            match+=n/2;
+            n=n/2;}
+        else{
+            match+=(n-1)/2;
+            n=(n-1)/2+1;
+}}
+
+return match;
+    }
+};
