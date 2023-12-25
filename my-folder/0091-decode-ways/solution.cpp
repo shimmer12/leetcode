d39a012ffc11ee62e64c4c3fd61896e876bffd66
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2023-12-25 05:53:45 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 6.5 MB (100.00%)

--- my-folder/0091-decode-ways/solution.cpp
+++ my-folder/0091-decode-ways/solution.cpp
@@ -1,22 +1,15 @@
-class Solution {
+class Solution { // 0 ms, faster than 100.00%
 public:
-	int numDecodings(string s) {
-		int n=s.size();
-		if(n==1){            // for length=1
-			if(s[0]>'0') return 1;
-			else return 0;
-		}
-		vector<int> dp(n);
-
-		for(int i=n-1;i>=0;--i){
-			if(s[i]>'0'){
-				dp[i]=(i+1<n)? dp[i+1]:1;
-				if(i+1<n && (s[i]=='1' || (s[i]<='2' && s[i+1]<='6'))){
-					 // if i=1 then i-1 can be from 0 to 9 OR i=2 then i-1 should be from 0 to 6
-					dp[i]+=(i+2<n)? dp[i+2]:1;
-				}
-			}
-		}
-		return dp[0];
-	}
+    int numDecodings(const string& s) {
+        int n = s.size();
+        vector<int> dp(n+1, 0);
+        dp[n] = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            if (s[i] != '0') // Single digit
+                dp[i] += dp[i+1];
+            if (i+1 < s.size() && (s[i] == '1' || s[i] == '2' && s[i+1] <= '6')) // Two digits
+                dp[i] += dp[i+2];
+        }
+        return dp[0];
+    }
 };
