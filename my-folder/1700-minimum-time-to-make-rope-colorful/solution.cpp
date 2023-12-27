b68b707756bb4f864d44e52fb847967a452bd26f
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2023-12-27 16:56:19 +0000

    [LeetCode Sync] Runtime - 127 ms (14.62%), Memory - 96 MB (100.00%)

--- my-folder/1700-minimum-time-to-make-rope-colorful/solution.cpp
+++ my-folder/1700-minimum-time-to-make-rope-colorful/solution.cpp
@@ -1,6 +1,12 @@
-class Solution {
+/*class Solution {
 public:
-    int minCost(string s, vector<int>& cost) {
+    int minCost(string colors, vector<int>& neededTime) {
+        
+    }
+};*/
+class Solution {
+    public:
+int minCost(string s, vector<int>& cost) {
     int res = cost[0], max_cost = cost[0];
     for (int i = 1; i < s.size(); ++i) {
         if (s[i] != s[i - 1]) {
