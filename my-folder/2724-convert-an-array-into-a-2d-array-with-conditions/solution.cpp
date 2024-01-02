716e40ac7b78c898052bcb6b5bd03a844b131ce8
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-01-02 13:47:11 +0000

    [LeetCode Sync] Runtime - 28 ms (5.23%), Memory - 29.8 MB (100.00%)

---
+++ my-folder/2724-convert-an-array-into-a-2d-array-with-conditions/solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+ //   vector<vector<int>> findMatrix(vector<int>& nums) {
+      vector<vector<int>> findMatrix(vector<int>& A) {
+        int n = A.size();
+        vector<int> count(n + 10);
+        vector<vector<int>> res;
+        for (int a : A) {
+            if (res.size() <= count[a])
+                res.push_back({});
+            res[count[a]++].push_back(a);
+        }
+        return res;
+      }  
+   // }
+};
