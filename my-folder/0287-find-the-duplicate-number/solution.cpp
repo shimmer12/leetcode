8bb993e47ec35533884bc4063f09df94f858184e
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-02-19 07:27:10 +0000

    [LeetCode Sync] Runtime - 39 ms (99.52%), Memory - 63.4 MB (93.86%)

--- my-folder/0287-find-the-duplicate-number/solution.cpp
+++ my-folder/0287-find-the-duplicate-number/solution.cpp
@@ -1,21 +1,32 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-      int hare=nums[0],tort=nums[0];
-
-      do{
-          hare=nums[nums[hare]];
-          tort=nums[tort];
-
-      }while(hare!=tort);
-
-
-      tort=nums[0];
-      while(hare!=tort){
-          hare=nums[hare];
-          tort=nums[tort];
-      }
-      return hare;
+        cin.tie(nullptr) -> sync_with_stdio(false);
+        for (int i : nums) {
+            int tar = abs(i);
+            if (nums[tar] < 0)
+                return tar;
+            nums[tar] = -nums[tar];
+        }
+        return 0;
     }
+};// class Solution {
+// public:
+//     int findDuplicate(vector<int>& nums) {
+//         int slow = nums[0];
+//         int fast = nums[0];
 
-};
+//         do {
+//             slow = nums[slow];
+//             fast = nums[nums[fast]];
+//         } while (slow != fast);
+
+//         slow = nums[0];
+//         while (slow != fast) {
+//             slow = nums[slow];
+//             fast = nums[fast];
+//         }
+
+//         return slow;
+//     }
+// };
