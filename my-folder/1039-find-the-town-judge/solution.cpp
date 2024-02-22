a4bfcee11cb32fb03b78894460a0d03b920b127f
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-02-22 17:02:30 +0000

    [LeetCode Sync] Runtime - 112 ms (84.91%), Memory - 64.2 MB (86.35%)

--- my-folder/1039-find-the-town-judge/solution.cpp
+++ my-folder/1039-find-the-town-judge/solution.cpp
@@ -1,36 +1,12 @@
 class Solution {
 public:
-   int findJudge(int n, vector<vector<int>>& trust) {
-        //this condition is to check if given input is empty but n==1
-		
-		if(trust.empty() && n==1)
-            return 1;
-		
-		//counts outdegree of a person or node 
-		//outdegree means number of nodes that are trusted by given specific node 
-        
-		vector<int> out(n+1,0);
-		
-		//counts indegree of a person or node 
-		//indegree means number of nodes that trusts a given specific node
-        
-		vector<int> in(n+1,0);
-		
-		//loop counts indegree and outdegree
-        
-		for(auto edge : trust){
-            out[edge[0]]++;
-            in[edge[1]]++;
+       int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> count(N + 1, 0);
+        for (auto& t : trust)
+            count[t[0]]--, count[t[1]]++;
+        for (int i = 1; i <= N; ++i) {
+            if (count[i] == N - 1) return i;
         }
-		
-		//if a node has outdegree==0 that means that node doesnot trust anyone and
-		//indegree ==n-1 that means all other nodes trusts this node except himself
-        
-		for(int i=0;i<=n;i++){
-            if(out[i]==0 && in[i]==n-1)
-                return i;
-        }
-		//if there is no such node return -1
         return -1;
     }
 };
