a8f8037a62e8713dc8a63e8c002d19c90f18b403
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-02-27 09:00:55 +0000

    [LeetCode Sync] Runtime - 4 ms (85.59%), Memory - 21.1 MB (64.82%)

--- my-folder/0026-remove-duplicates-from-sorted-array/solution.cpp
+++ my-folder/0026-remove-duplicates-from-sorted-array/solution.cpp
@@ -1,10 +1,14 @@
 class Solution {
 public:
-   int removeDuplicates(vector<int>& nums) {
-    int i = 0;
-    for (int n : nums)
-        if (!i || n > nums[i-1])
-            nums[i++] = n;
-    return i;
+    int removeDuplicates(vector<int>& nums) {
+        int i=0;
+        int n=nums.size();
+        for(int j=1;j<n;j++)
+        if(nums[i]!=nums[j]){
+        i++;
+        nums[i]=nums[j]; //to overwrite dup value with unique values
+
 }
+        return i+1;
+    }
 };
