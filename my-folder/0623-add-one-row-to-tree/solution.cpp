f5ad9c163847a183833c8a278176fa658986c701
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-04-16 14:19:29 +0000

    [LeetCode Sync] Runtime - 10 ms (81.91%), Memory - 24.4 MB (25.41%)

--- my-folder/0623-add-one-row-to-tree/solution.cpp
+++ my-folder/0623-add-one-row-to-tree/solution.cpp
@@ -1,15 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* addOneRow(TreeNode* root, int v, int d) {
-        if (d == 0 || d == 1) {
-            TreeNode* newroot = new TreeNode(v);
-            (d ? newroot->left : newroot->right) = root;
-            return newroot;
-        }
-        if (root && d >= 2) {
-            root->left  = addOneRow(root->left,  v, d > 2 ? d - 1 : 1);
-            root->right = addOneRow(root->right, v, d > 2 ? d - 1 : 0);
-        }
-        return root;
+map<TreeNode*,bool>storeLevel;
+void markAll(TreeNode*root,int level,int depth){
+    if(!root){
+        return;
     }
+    if(level == depth - 1){
+        storeLevel[root] = true;
+        return;
+    }
+    
+    markAll(root->left,level + 1,depth);
+    markAll(root->right,level + 1,depth);
+}
+TreeNode* helper(TreeNode*root,int val){
+    if(!root){
+        return NULL;
+    }
+    if(storeLevel.find(root)!=storeLevel.end()){
+        TreeNode*templeft = new TreeNode(val);
+        TreeNode*tempright = new TreeNode(val);
+        templeft->left = root->left;
+        tempright->right = root->right;
+        root->left = templeft;
+        root->right = tempright;
+        return root; // very important for time complexity improvement!!
+    }
+    root->left = helper(root->left,val);
+    root->right = helper(root->right,val);
+    return root;
+}
+TreeNode*addOneRow(TreeNode*root,int val,int depth){
+    if(!root){
+        return NULL;
+    }
+    if(depth == 1){
+        TreeNode*temp  = new TreeNode(val);
+        temp->left = root;
+        return temp;
+    }
+    int level = 1;
+    markAll(root,level,depth);
+    root = helper(root,val);
+    return root;
+}
 };
