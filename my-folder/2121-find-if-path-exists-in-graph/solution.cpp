1e035134785afdae6bbd9bfa7ec73395f3d31a92
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-04-21 05:42:07 +0000

    [LeetCode Sync] Runtime - 195 ms (99.73%), Memory - 125.4 MB (85.69%)

--- my-folder/2121-find-if-path-exists-in-graph/solution.cpp
+++ my-folder/2121-find-if-path-exists-in-graph/solution.cpp
@@ -1,32 +1,31 @@
 class Solution {
+    vector<int> leader;
+    vector<int> size;
 public:
-    bool validPath(int n, vector<vector<int>>& edges, int start, int end) {
-        vector<vector<int>> graph(n);
-        // Build the graph
-        for(int i=0; i<edges.size(); i++) {
-            graph[edges[i][0]].push_back(edges[i][1]);
-            graph[edges[i][1]].push_back(edges[i][0]);
-        }
-        
-        // BFS
-        queue<int> q;
-        vector<int> visited(n, 0);
-        q.push(start);
-        visited[start] = 1;
-        while(!q.empty()){
-            int top = q.front();
-            q.pop();
-            if(top == end)
-                return true;
-
-            for(int i=0; i<graph[top].size(); i++){
-                if(visited[graph[top][i]] == 0){
-                    q.push(graph[top][i]);
-                    visited[graph[top][i]] = 1;
-                }
-            }
-        }
-        return false;
+    int find(int u) {
+        while (u != leader[u])
+            u = leader[u];
+        return u;
+    }
+    void combine(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u==v)
+            return;
+        if (size[u]>size[v])
+            swap(u, v);
+        leader[u] = v;
+        size[v] += size[u];
+    }
+    bool validPath(const int n, const vector<vector<int>>& edges, const int source, const int destination) {
+        ios::sync_with_stdio(0);
+        cin.tie(0); cout.tie(0);
+        leader = vector<int>(n);
+        size = vector<int>(n, 1);
+        for (int i=0; i<n; i++)
+            leader[i] = i;
+        for (const auto& edge : edges)
+            combine(edge[0], edge[1]);
+        return find(source)==find(destination);
     }
 };
-
