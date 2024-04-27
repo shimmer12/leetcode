acda215fe046450ed5db64d2587f953d94b2c7c7
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-04-27 15:55:35 +0000

    [LeetCode Sync] Runtime - 263 ms (65.32%), Memory - 44.3 MB (45.05%)

---
+++ my-folder/3406-find-all-possible-stable-binary-arrays-i/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+   int numberOfStableArrays(int zero, int one, int limit) {
+       const int MOD = 1000000007;
+        vector<vector<vector<long long>>> dp(one + 1, vector<vector<long long>>(zero + 1, vector<long long>(2, 0)));
+        dp[0][0][0] = dp[0][0][1] = 1;
+
+        for (int i = 0; i <= one; ++i) {
+            for (int j = 0; j <= zero; ++j) {
+                for (int k = 1; k <= limit; ++k) {
+                    if (i - k >= 0) // Ensure i - k is within bounds
+                        dp[i][j][1] = (dp[i][j][1] + dp[i - k][j][0]) % MOD;
+                    
+                    if (j - k >= 0)
+                        dp[i][j][0] = (dp[i][j][0] + dp[i][j - k][1]) % MOD;
+                }
+            }
+        }
+
+        return static_cast<int>((dp[one][zero][0] + dp[one][zero][1]) % MOD);
+    }
+};
