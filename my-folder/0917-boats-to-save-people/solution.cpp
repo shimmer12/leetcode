65be0e9b22aef0560f22f1fd0b6907618dde9f97
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-05-04 06:07:12 +0000

    [LeetCode Sync] Runtime - 40 ms (95.69%), Memory - 45.6 MB (5.19%)

--- my-folder/0917-boats-to-save-people/solution.cpp
+++ my-folder/0917-boats-to-save-people/solution.cpp
@@ -1,23 +1,39 @@
+const int ZERO = []()
+{
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(nullptr);
+	return 0;
+}();
+
 class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
-         int n= people.size();
-         int left=0,right=n-1, boat=0;
-        sort(people.begin(),people.end());
+        sort(people.begin(), people.end());
+        int count = 0;
+        int left = 0, right = people.size() - 1;
 
-       
+        // while (left <= right) {
+        //     if (people[left] + people[right] <= limit) {
+        //         left++;
+        //     }
+        //     right--;
+        //     count++;
+        // }
         while(left<=right)
-               if( people[left]+people[right]<=limit){
-                    boat++;
-                    right--;
-                    left++;
-               }
-                    else {
-                        boat++;
-                        right--;
-                    }
-            
-            
-        return boat;
+        {
+            if(people[left]+people[right]<=limit)
+            {
+                count++;
+                left++;
+                right--;
+            }
+            else
+            {
+                count++;
+                right--;
+            }
+        }
+
+        return count;
     }
 };
