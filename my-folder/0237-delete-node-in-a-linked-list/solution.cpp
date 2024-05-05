f9a7651b09839e78eb3cb7ad9f7281f739577636
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-05-05 10:15:38 +0000

    [LeetCode Sync] Runtime - 16 ms (6.46%), Memory - 11.2 MB (98.28%)

--- my-folder/0237-delete-node-in-a-linked-list/solution.cpp
+++ my-folder/0237-delete-node-in-a-linked-list/solution.cpp
@@ -9,6 +9,12 @@
 class Solution {
 public:
     void deleteNode(ListNode* node) {
-    *node = *node->next;
-}
+        node->val=(node->next)->val;
+        while((node->next)->next!=NULL){
+            (node->next)->val=((node->next)->next)->val;
+            cout<<node->val<<"->";
+            node=node->next;
+        }
+        node->next=NULL;
+    }
 };
