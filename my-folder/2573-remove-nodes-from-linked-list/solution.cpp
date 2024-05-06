aff44ec0f3951b74ce57269ab1f1ab74981390d4
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-05-06 09:20:36 +0000

    [LeetCode Sync] Runtime - 202 ms (99.18%), Memory - 164.4 MB (75.26%)

---
+++ my-folder/2573-remove-nodes-from-linked-list/solution.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* removeNodes(ListNode* head) {
+        ios_base::sync_with_stdio(0);
+        if(head==NULL)return head;
+        head->next = removeNodes(head->next);
+        if(head->next==NULL)return head;
+        if(head->next->val>head->val)return head->next;
+        return head;
+    }
+};
