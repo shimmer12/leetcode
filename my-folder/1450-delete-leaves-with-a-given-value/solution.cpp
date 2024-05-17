6e4f3c217a59cc79cb249810bf75c15964b1f6ec
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-05-17 14:39:19 +0000

    [LeetCode Sync] Runtime - 10 ms (68.40%), Memory - 21 MB (96.59%)

---
+++ my-folder/1450-delete-leaves-with-a-given-value/solution.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* r, int target) {
+    if (r == nullptr) return nullptr;
+    r->left = removeLeafNodes(r->left, target);
+    r->right = removeLeafNodes(r->right, target);
+    return (r->left == r->right && r->val == target) ? nullptr : r;
+}
+};
