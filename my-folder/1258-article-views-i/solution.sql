542e02961176318eb3ef998dcbea4accf24c4cdf
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-06-14 23:55:50 +0000

    [LeetCode Sync] Runtime - 402 ms (90.68%), Memory - 0B (100.00%)

---
+++ my-folder/1258-article-views-i/solution.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+
+Select distinct author_id as id from Views where author_id=viewer_id order by id asc
+-- select distict unique values
+-- order by to sort on ascending 
+-- as in renames
+-- select distinct authotid as id   from views where authid=viewerid order by id asc 
