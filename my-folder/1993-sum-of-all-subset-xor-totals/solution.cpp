fba1455ec71b3f68d4cc4fe769950a6fc227f95c
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-06-18 03:07:49 +0000

    [LeetCode Sync] Runtime - 94 ms (5.10%), Memory - 46.8 MB (5.01%)

--- my-folder/1993-sum-of-all-subset-xor-totals/solution.cpp
+++ my-folder/1993-sum-of-all-subset-xor-totals/solution.cpp
@@ -1,6 +1,32 @@
 class Solution {
 public:
-   int subsetXORSum(vector<int>& n) {
-    return accumulate(begin(n), end(n), 0, bit_or<int>()) << (n.size() - 1);
-}
+
+    int subsetXORSum(vector<int>& n) {
+       // int m=n.size();
+        vector<vector<int>>su;
+        f(n,0,{},su);
+        int res=0;
+        for(auto & s:su){
+            int xo=0;
+            for(int num:s){
+                xo^=num;
+            }
+            res+=xo;
+        }
+        return res;
+    }
+
+    private:
+
+    void f(const vector<int>&n, int idx,vector<int>s, vector<vector<int>>& su){
+        if(idx==n.size()){
+            su.push_back(s);
+            return ;
+        }
+        s.push_back(n[idx]);// subset w/ n[i] element
+        f(n,idx+1,s,su);
+        s.pop_back();
+
+        f(n,idx+1,s,su);//subset w/o n[i]ele
+    }
 };
