fa2fe02c9be18803e8fa4dbba039c384793d71e4
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-06-19 08:28:00 +0000

    [LeetCode Sync] Runtime - 4 ms (24.28%), Memory - 7.7 MB (18.83%)

--- my-folder/1894-merge-strings-alternately/solution.cpp
+++ my-folder/1894-merge-strings-alternately/solution.cpp
@@ -1,10 +1,20 @@
 class Solution {
 public:
     string mergeAlternately(string w1, string w2) {
-    int i = 0, j = 0;
-    string res;
-    while (i < w1.size() && j < w2.size())
-        res += string() + w1[i++] + w2[j++];
-    return res + w1.substr(i) + w2.substr(j);
-}
+        int n=w1.size();
+        int m=w2.size();
+        string res="";
+        int i=0;//j=0;
+        while(i<n||i<m){
+            if(i<n){
+                res+=w1[i];
+            }
+            if(i<m){
+res+=w2[i];
+            }
+            i++;
+        }
+
+return res;
+    }
 };
