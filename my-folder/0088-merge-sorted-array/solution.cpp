603aa97ccb1f8f651922ae021cd83a678eaca3b0
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-06-19 10:59:18 +0000

    [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 11 MB (71.93%)

--- my-folder/0088-merge-sorted-array/solution.cpp
+++ my-folder/0088-merge-sorted-array/solution.cpp
@@ -1,24 +1,25 @@
-
-
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-       int i=m-1;
-       int j=n-1;
-       int k=m+n-1;
+    void merge(vector<int>& n1, int m, vector<int>& n2, int n) {
+       int i=m-1,j=n-1,k=m+n-1;
        while(i>=0 && j>=0){
-           if(nums1[i]>nums2[j]){
-           nums1[k]=nums1[i];
-           i--;k--;
-}
-           else{ 
-           nums1[k]=nums2[j];
-           k--;j--;}}
-        while(j>=0){
-            nums1[k]=nums2[j];
-            k--;j--;
+        if(n1[i]>n2[j]){
+            //                             i     k         j
+            //ele n1 >n2 last eles    [1,2,3,0,0,0]    2 5 6
+            n1[k]=n1[i];
+            //put ele big at end 
+            i--;k--; 
         }
-       }
+        else{
+            n1[k]=n2[j];
+            j--;k--;
 
-    
+        }}
+        while(j>=0){
+n1[k]=n2[j];
+j--;k--;
+        
+       }
+        
+    }
 };
