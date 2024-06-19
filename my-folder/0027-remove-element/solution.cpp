af08bb8cccab9e7a2c86a14ec1d2824b4fcdb024
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-06-19 19:13:49 +0000

    [LeetCode Sync] Runtime - 5 ms (27.69%), Memory - 10.4 MB (43.20%)

--- my-folder/0027-remove-element/solution.cpp
+++ my-folder/0027-remove-element/solution.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
-   int removeElement(vector<int>& nums, int val) {
-    int cnt = 0;
-    for(int i = 0 ; i < nums.size() ; ++i) {
-        if(nums[i] == val)
-            cnt++;
-        else
-            nums[i-cnt] = nums[i];
+    int removeElement(vector<int>& nums, int val) {
+        int m = nums.size();
+        int s = 0;
+        int i = 0, j = m - 1;
+
+       for(int i=0;i<m;i++){
+        if(nums[i]!=val){
+            nums[s++]=nums[i];
+        }
+
+       }return s;
     }
-    return nums.size()-cnt;
-}
 };
+
