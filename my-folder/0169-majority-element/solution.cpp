f2609cef80f5b5c512bdd7aded380454034468b7
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-06-21 07:45:58 +0000

    [LeetCode Sync] Runtime - 19 ms (49.42%), Memory - 26.8 MB (46.83%)

--- my-folder/0169-majority-element/solution.cpp
+++ my-folder/0169-majority-element/solution.cpp
@@ -1,12 +1,21 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        unordered_map<int, int> counter;
-        for (int num : nums) {
-            if (++counter[num] > nums.size() / 2) {
-                return num;
-            }
+     int n=nums.size();
+     int m=floor(n/2);
+        map<int,int>mp;
+        for(int i=0;i<n;i++){
+            mp[nums[i]]++;
+
+        }
+        for(auto it:mp){
+                if(it.second>m){
+                    return it.first;
+                }
         }
         return 0;
+
+        //nlogn+n
+        //n
     }
 };
