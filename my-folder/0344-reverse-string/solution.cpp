f8b79b230d5a94e72812602e14101eff725e23f0
Srishti Soni <92056170+shimmer12@users.noreply.github.com>
2024-06-22 11:50:22 +0000

    [LeetCode Sync] Runtime - 16 ms (66.96%), Memory - 26.9 MB (58.85%)

--- my-folder/0344-reverse-string/solution.cpp
+++ my-folder/0344-reverse-string/solution.cpp
@@ -1,11 +1,6 @@
 class Solution {
-
 public:
-    void reverseString(vector<char>& s) {
-        int j=0;
-        int e=s.size()-1;
-        while(j<e){
-            swap(s[j++],s[e--]);
-        }
+    void reverseString(vector<char>&s){
+        reverse(s.begin(),s.end());
     }
 };
